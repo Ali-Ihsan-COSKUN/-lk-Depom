2a370c1bd5638f184aa9481bfeb89fdf7a2554e4
Ali-Ihsan-COSKUN <161217293+Ali-Ihsan-COSKUN@users.noreply.github.com>
2024-02-25 16:31:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+for i in meme_dict.keys():
+    print(meme_dict[i])
